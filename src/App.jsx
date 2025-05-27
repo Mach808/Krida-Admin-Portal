247cedff01ac0ddad09b6ac11d410eb00573b4d1
Manan Choudhary <mananchoudhary1995@gmail.com>
2025-05-28 00:27:43 +0530

    Update App.jsx
    reverted changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,28 @@ import Sports from './Components/screen/Sports'
 import Receipts from './Components/screen/Receipts'
 import EditCourts from './Components/screen/EditCourt'
 import Settings from './Components/settings/settings'
-import TeamBooking from './Components/screen/TeamBooking'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, HashRoute } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
+import TeamBooking from './Components/screen/TeamBooking.jsx'
 
 function App() {
+  const location = useLocation();
+  
   return (
-    <Router basename='/Krida-Admin-Portal/'>
-      <Routes>
-        <Route path="/" element={<AuthPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/equipments" element={<Equipments />} />
-        <Route path="/bookings" element={<Bookings />} />
-        <Route path="/courts" element={<Courts />} />
-        <Route path="/sports" element={<Sports />} />
-        <Route path="/receipts" element={<Receipts />} />
-        <Route path="/edit" element={<EditCourts />} />
-        <Route path="/settingsPage" element={<Settings />} />
-        <Route path="/teambookings" element={<TeamBooking />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<AuthPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/equipments" element={<Equipments />} />
+      <Route path="/bookings" element={<Bookings />} />
+      <Route path="/courts" element={<Courts />} />
+      <Route path="/sports" element={<Sports />} />
+      <Route path="/receipts" element={<Receipts />} />
+      <Route path="/edit" element={<EditCourts />} />
+      <Route path="/settingsPage" element={<Settings />} />
+      <Route path = "/teambookings" element={<TeamBooking/>} />
+    </Routes>
   )
 }
 
 export default App
+can u fix it
