c2bad249f9da65745b3cec1c7d19f1456cee470b
Manan Choudhary <mananchoudhary1995@gmail.com>
2025-05-28 00:21:18 +0530

    Update App.jsx
    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,25 @@ import Sports from './Components/screen/Sports'
 import Receipts from './Components/screen/Receipts'
 import EditCourts from './Components/screen/EditCourt'
 import Settings from './Components/settings/settings'
-import { Routes, Route } from 'react-router-dom'
-import { useLocation } from 'react-router-dom';
-import TeamBooking from './Components/screen/TeamBooking.jsx'
+import TeamBooking from './Components/screen/TeamBooking'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const location = useLocation();
-  
   return (
-    <Routes>
-      <Route path="/" element={<AuthPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/equipments" element={<Equipments />} />
-      <Route path="/bookings" element={<Bookings />} />
-      <Route path="/courts" element={<Courts />} />
-      <Route path="/sports" element={<Sports />} />
-      <Route path="/receipts" element={<Receipts />} />
-      <Route path="/edit" element={<EditCourts />} />
-      <Route path="/settingsPage" element={<Settings />} />
-      <Route path = "/teambookings" element={<TeamBooking/>} />
-    </Routes>
+    <Router basename='/Krida-Admin-Portal/'>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/equipments" element={<Equipments />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/courts" element={<Courts />} />
+        <Route path="/sports" element={<Sports />} />
+        <Route path="/receipts" element={<Receipts />} />
+        <Route path="/edit" element={<EditCourts />} />
+        <Route path="/settingsPage" element={<Settings />} />
+        <Route path="/teambookings" element={<TeamBooking />} />
+      </Routes>
+    </Router>
   )
 }
 
